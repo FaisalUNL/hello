ce338c1c176735d975c87a9a8c347473860e348f
FaisalUNL <falmaaini2@huskers.unl.edu>
2021-08-27 20:14:53 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Authors: Faisal Al Maaini / Zaiden De La O
+ *         falmaaini2@huskers.unl.edu
+ * Date: 2020/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Faisal Maaini\n");
+
+
+  return 0;
+}
