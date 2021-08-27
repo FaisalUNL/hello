ad949970ecb299d9d8e11472766ea256e61fd928
BigZ5709 <zdelao3@huskers.unl.edu>
2021-08-27 20:37:47 +0000

    added my own information

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 /**
  * Authors: Faisal Al Maaini / Zaiden De La O
- *         falmaaini2@huskers.unl.edu
- * Date: 2020/08/24
+ *         falmaaini2@huskers.unl.edu / zdelao3@huskers.unl.edu
+ * Date: 2020/08/27
  *
  * A simple hello world program in C
  *
@@ -13,6 +13,8 @@ int main(int argc, char **argv) {
 
   printf("Faisal Maaini!\n");
   printf("Computer Science\n");
+  printf("Zaiden De La O!\n");
+  printf("Undeclared Major!\n");
 
   return 0;
 }
